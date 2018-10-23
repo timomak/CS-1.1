c2aaac113d4203973316bcd253c09e28116fa805
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-23 11:03:53 -0700

    Not sure what is not working.

--- checklist/checklist.py
+++ checklist/checklist.py
@@ -37,6 +37,7 @@ def list_all_items():
         elif completed[index] == 1:
             print("[V] " + "{} {} ".format(index, list_item))
         index += 1
+
 def mark_completed(index):
     complted.pop(index)
     completed.insert(index, 1)
@@ -44,12 +45,12 @@ def mark_completed(index):
 
     print(completed[index])
 
-def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
-    newUser_input = input(prompt)
-    return str(newUser_input)
-
+# def user_input(prompt):
+#     # the input function will display a message in the terminal
+#     # and wait for user input.
+#     newUser_input = input(prompt)
+#     return str(newUser_input)
+#
 def user_numInput(prompt):
     newUser_input = input(prompt)
     if isinstance(newUser_input, str):
@@ -59,11 +60,10 @@ def user_numInput(prompt):
 def select(function_code):
     # Create item
     if function_code == "A":
-        new_item = user_input("Add to list:")
+        new_item = input("Add to list:")
         if type(new_item) is str:
             create(new_item)
         # else select("A")
-
     # Read item
     elif function_code == "R":
         item_index = user_numInput("Index Number to remove: ")
@@ -72,11 +72,12 @@ def select(function_code):
 
     elif function_code == "U":
         item_index = user_numInput("Index Number you'd like to update: ")
-        new_item = user_input("New item name: ")
+        new_item = input("New item name: ")
         update(item_index, new_item)
 
     elif function_code == "C":
         items_index = user_numInput("Completed item: ")
+        print(item_index)
         mark_completed(items_index)
     # Print all items
     elif function_code == "P":
@@ -86,40 +87,44 @@ def select(function_code):
     else:
         print("Unknown Option")
 
-while running == True:
-    selection = user_input("A to add to list\nR to Remove from list\nU to update item\nC to complete item\nP to display list\nQ to exit.\n")
-    if selection == "Q" or selection == "q":
-        running = False
-    else:
-        select(selection.upper())
-# def test():
-#     # Your testing code here
-#     create("purple sox")
-#     create("red cloak")
-#     list_all_items()
-#     print (completed)
-#     #
-#     # print(read(0))
-#     # print(read(1))
-#     #
-#     # update(0, "purple socks")
-#     #
-#     # destroy(1)
-#     #
-#     # print(read(0))
-#     #
-#     # mark_completed(0)
-#     # list_all_items()
-#     # Call your new function with the appropriate value
-#     select("C")
-#     # View the results
-#     # list_all_items()
-#     # Call function with new value
-#     # select("R")
-#     # # View results
-#     # list_all_items()
-#     # Continue until all code is run
-#     # select("P")
-#     # list_all_items()
-#     # select("Q")
-# test()
+# while running == True:
+#     selection = input("A to add to list\nR to Remove from list\nU to update item\nC to complete item\nP to display list\nQ to exit.\nInput: ")
+#     print("Selection: ", selection)
+#     if selection == "Q" or selection == "q":
+#         running = False
+#     else:
+#         select(selection.upper())
+
+
+def test():
+    # Your testing code here
+    create("purple sox")
+    create("red cloak")
+    list_all_items()
+    print (completed)
+    #
+    # print(read(0))
+    # print(read(1))
+    #
+    # update(0, "purple socks")
+    #
+    # destroy(1)
+    #
+    # print(read(0))
+    #
+    # mark_completed(0)
+    # list_all_items()
+    # Call your new function with the appropriate value
+    select("C")
+    # View the results
+    # list_all_items()
+    # Call function with new value
+    # select("R")
+    # # View results
+    # list_all_items()
+    # Continue until all code is run
+    # select("P")
+    # list_all_items()
+    # select("Q")
+
+test()
